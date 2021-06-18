ec5b6f1a177be02e1c8372253d3582ebb2841333
Mateus Cardoso <mateusrock.cardoso@gmail.com>
2021-06-18 09:55:16 -0300

    increase comments hidden

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-CreateTopic = (displayBlock, displayNone) =>{
+CreateTopic = (displayBlock, displayNone) =>{ 
         let changeToNone = document.querySelector(`.${displayBlock}`)
         let chageToBlock =  document.querySelector(`.${displayNone}`)
 
         changeToNone.style.display = 'none'
         chageToBlock.style.display = 'block'
+        
 }
+
+ChangeDisplay = (display) =>{
+        let changeDisplay = document.querySelector(`.${display}`)
+
+        if(changeDisplay.style.display == 'none'){
+                changeDisplay.style.display = 'block'
+        }else{
+                changeDisplay.style.display = 'none'
+        }
+}
+
+Expand = () =>{
+        let text = "<b>aloooo</b>"
+        let verMais = document.querySelector(`.text-abstract`)
+        verMais.innerHTML + text
+}
+
+
+
+
+
