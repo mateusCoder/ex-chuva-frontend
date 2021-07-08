db49797516bb949c05b84d56c6f117730b392a12
Mateus Cardoso <mateusrock.cardoso@gmail.com>
2021-07-08 12:46:37 -0300

    add button effects

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-CreateTopic = (displayBlock, displayNone) =>{ 
-        let changeToNone = document.querySelector(`.${displayBlock}`)
-        let chageToBlock =  document.querySelector(`.${displayNone}`)
+CreateTopic = (displayBlock, displayNone) => {
+   let changeToNone = document.querySelector(`.${displayBlock}`)
+   let chageToBlock = document.querySelector(`.${displayNone}`)
 
-        changeToNone.style.display = 'none'
-        chageToBlock.style.display = 'block' 
+   changeToNone.style.display = 'none'
+   chageToBlock.style.display = 'block'
 }
 
-ChangeDisplay = (display) =>{
-        let changeDisplay = document.querySelector(`.${display}`)
+ChangeDisplay = (display) => {
+   let changeDisplay = document.querySelector(`.${display}`)
 
-        if(changeDisplay.style.display == 'none'){
-                changeDisplay.style.display = 'block'
-        }else{
-                changeDisplay.style.display = 'none'
-        }
+   if (changeDisplay.style.display == 'none') {
+      changeDisplay.style.display = 'block'
+   } else {
+      changeDisplay.style.display = 'none'
+   }
+}
+
+Expand = () => {
+   const seeMore = document.querySelector('.see-more')
+   const textAbstract = document.querySelector('.text-abstract')
+
+   textAbstract.style.height = 'auto'
+   seeMore.style.display = 'none'
 }
 
 
